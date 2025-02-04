6e0c9642c74fe4a72904a5a127fac8ab589c2693
Buckyatplay <rvf_7585@hotmail.com>
2025-02-04 21:11:09 +0800

    Choon Yee activity 3-1

---
+++ backend.tf
@@ -0,0 +1,9 @@
+/*
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce8-tfstate"
+    region = "ap-southeast-1"
+    key = "choonyee3-1activity.tfstate" #Name of your tfstate file. It must be different from other projects 
+  }
+  }
+*/
