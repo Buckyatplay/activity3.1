de0e65af015df9ea225154082b52d9b9222a5100
Buckyatplay <rvf_7585@hotmail.com>
2025-02-04 22:13:36 +0800

    Choon Yee activity 3-1

--- main.tf
+++ main.tf
@@ -8,17 +8,3 @@ resource "aws_s3_bucket" "static_bucket" {
  force_destroy = true
 }
 
-## EC2 Instance
-resource "aws_instance" "dynamodb_reader" {
-  ami                    = "ami-04c913012f8977029"
-  instance_type          = "t2.micro"
-  subnet_id              = data.aws_subnets.public.ids[0]
-  vpc_security_group_ids = [aws_security_group.dynamodb_reader.id]
-  associate_public_ip_address = true
-
-  iam_instance_profile = aws_iam_instance_profile.profile_dynamodb.id
-
-  tags = {
-    Name = "${local.name_prefix}-dynamodb-reader"
-  }
-}
